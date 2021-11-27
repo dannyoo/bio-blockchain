dcd019af60c6df0b747f6e2709f0dc04e6484b1e
Daniel Jimenez <daniel.jimenez@jhuapl.edu>
2021-11-27 12:33:07 -0500

    readFasta files

---
+++ fasta.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "bufio"
+    "log"
+    "os"
+)
+
+func readFasta() (label, seq string){
+    // open file
+    f, err := os.Open("file.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    // remember to close the file at the end of the program
+    defer f.Close()
+
+    // read the file line by line using scanner
+    scanner := bufio.NewScanner(f)
+
+    for scanner.Scan() {
+        // do something with a line
+		txt := scanner.Text()
+		if txt[0] == '>'{
+			label = txt[1:]
+		} else {
+			seq = txt
+		}
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+	return
+}
